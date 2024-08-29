d08048d8a230e679f797527eca4e817f0d7dc96c
RICHLOVER12 <145176909+RICHLOVER12@users.noreply.github.com>
2024-08-28 20:30:17 -0600

    Add files via upload

---
+++ BDD_VERA.sql
@@ -0,0 +1,332 @@
+--------------------------------------------------------
+-- Archivo creado  - miércoles-agosto-28-2024   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table TBL_VOTACIONES
+--------------------------------------------------------
+
+  CREATE TABLE "TBL_VOTACIONES" 
+   (	"ID_VOTACIONES" NUMBER(*,0), 
+	"NOMBRE_VOTACIONES" VARCHAR2(30 BYTE), 
+	"DESCRIPCION_VOTACIONES" VARCHAR2(50 BYTE), 
+	"FECHA_VOTACIONES" DATE, 
+	"IMG_VOTACIONES" VARCHAR2(30 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+--------------------------------------------------------
+--  DDL for Table TBL_VOTACION
+--------------------------------------------------------
+
+  CREATE TABLE "TBL_VOTACION" 
+   (	"ID_VOTO" NUMBER(*,0), 
+	"ID_USUARIO" NUMBER(*,0), 
+	"ID_PARTIDO" NUMBER(*,0)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+--------------------------------------------------------
+--  DDL for Table TBL_PARTIDO
+--------------------------------------------------------
+
+  CREATE TABLE "TBL_PARTIDO" 
+   (	"ID_PARTIDO" NUMBER(*,0), 
+	"NOMBRE_PARTIDO" VARCHAR2(30 BYTE), 
+	"NOMBRE_CANDIDATOP" VARCHAR2(30 BYTE), 
+	"NOMBRE_DESIGNADO1" VARCHAR2(30 BYTE), 
+	"NOMBRE_DESIGNADO2" VARCHAR2(30 BYTE), 
+	"NOMBRE_DESIGNADO3" VARCHAR2(30 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+--------------------------------------------------------
+--  DDL for Table USUARIOS
+--------------------------------------------------------
+
+  CREATE TABLE "USUARIOS" 
+   (	"ID_USUARIO" NUMBER(*,0), 
+	"CLAVE" VARCHAR2(20 BYTE), 
+	"NOMBRE2_USUARIO" VARCHAR2(20 BYTE), 
+	"APELLIDO1_USUARIO" VARCHAR2(20 BYTE), 
+	"APELLIDO2_USUARIO" VARCHAR2(20 BYTE), 
+	"DNI_USUARIO" NUMBER(*,0), 
+	"CONTRASEÑA_USUARIO" VARCHAR2(30 BYTE), 
+	"IMG_USUARIO" VARCHAR2(30 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+--------------------------------------------------------
+--  DDL for Table TBL_GRAFICA
+--------------------------------------------------------
+
+  CREATE TABLE "TBL_GRAFICA" 
+   (	"ID_GRAFICA" NUMBER(*,0), 
+	"ID_PARTIDO" NUMBER(*,0), 
+	"TOTALVOTOS_GRAFICA" NUMBER(*,0), 
+	"GANADOR_GRAFICA" VARCHAR2(20 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+--------------------------------------------------------
+--  DDL for Table TBL_DVOTACIONES
+--------------------------------------------------------
+
+  CREATE TABLE "TBL_DVOTACIONES" 
+   (	"ID_DVOTACIONES" NUMBER(*,0), 
+	"NOMBRE_DVOTACIONES" VARCHAR2(20 BYTE), 
+	"DESC_DVOTACIONES" VARCHAR2(50 BYTE), 
+	"CANTVOTOS" NUMBER(*,0), 
+	"PORCENTAJE_VOTOS" NUMBER(*,0), 
+	"RESULTADO_DVOTACIONES" VARCHAR2(20 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+--------------------------------------------------------
+--  DDL for Table TBL_CANDIDATOS
+--------------------------------------------------------
+
+  CREATE TABLE "TBL_CANDIDATOS" 
+   (	"ID_CANDIDATO" NUMBER(*,0), 
+	"NOMBRE_CANDIDATO" VARCHAR2(20 BYTE), 
+	"NOMBRE_PARTIDO" VARCHAR2(20 BYTE), 
+	"NOMBRE_DESIGN1" VARCHAR2(20 BYTE), 
+	"NOMBRE_DESIGN2" VARCHAR2(20 BYTE), 
+	"NOMBRE_DESIGN3" VARCHAR2(20 BYTE), 
+	"IMG_CANDIDATO" VARCHAR2(200 BYTE), 
+	"IMG_DESIGN1" VARCHAR2(200 BYTE), 
+	"IMG_DESIGN2" VARCHAR2(200 BYTE), 
+	"IMG_DESIGN3" VARCHAR2(200 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+--------------------------------------------------------
+--  DDL for View MVIEW_EVALUATIONS
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE NONEDITIONABLE VIEW "MVIEW_EVALUATIONS" ("RUNID", "MVIEW_OWNER", "MVIEW_NAME", "RANK", "STORAGE_IN_BYTES", "FREQUENCY", "CUMULATIVE_BENEFIT", "BENEFIT_TO_COST_RATIO") AS 
+  select
+  t1.runid# as runid,
+  summary_owner AS mview_owner,
+  summary_name AS mview_name,
+  rank# as rank,
+  storage_in_bytes,
+  frequency,
+  cumulative_benefit,
+  benefit_to_cost_ratio
+from SYSTEM.MVIEW$_ADV_OUTPUT t1, SYSTEM.MVIEW$_ADV_LOG t2, ALL_USERS u
+where
+  t1.runid# = t2.runid# and
+  u.username = t2.uname and
+  u.user_id = userenv('SCHEMAID') and
+  t1.output_type = 1
+order by t1.rank#;
+
+   COMMENT ON TABLE "MVIEW_EVALUATIONS"  IS 'This view gives DBA access to summary evaluation output'
+;
+--------------------------------------------------------
+--  DDL for View MVIEW_EXCEPTIONS
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE NONEDITIONABLE VIEW "MVIEW_EXCEPTIONS" ("RUNID", "OWNER", "TABLE_NAME", "DIMENSION_NAME", "RELATIONSHIP", "BAD_ROWID") AS 
+  select
+  t1.runid# as runid,
+  owner,
+  table_name,
+  dimension_name,
+  relationship,
+  bad_rowid
+from SYSTEM.MVIEW$_ADV_EXCEPTIONS t1, SYSTEM.MVIEW$_ADV_LOG t2, ALL_USERS u
+where
+  t1.runid# = t2.runid# and
+  u.username = t2.uname and
+  u.user_id = userenv('SCHEMAID');
+
+   COMMENT ON TABLE "MVIEW_EXCEPTIONS"  IS 'This view gives DBA access to dimension validation results'
+;
+--------------------------------------------------------
+--  DDL for View MVIEW_FILTER
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE NONEDITIONABLE VIEW "MVIEW_FILTER" ("FILTERID", "SUBFILTERNUM", "SUBFILTERTYPE", "STR_VALUE", "NUM_VALUE1", "NUM_VALUE2", "DATE_VALUE1", "DATE_VALUE2") AS 
+  select
+      a.filterid# as filterid,
+      a.subfilternum# as subfilternum,
+      decode(a.subfiltertype,1,'APPLICATION',2,'CARDINALITY',3,'LASTUSE',
+                             4,'FREQUENCY',5,'USER',6,'PRIORITY',7,'BASETABLE',
+                             8,'RESPONSETIME',9,'COLLECTIONID',10,'TRACENAME',
+                             11,'SCHEMA','UNKNOWN') AS subfiltertype,
+      a.str_value,
+      to_number(decode(a.num_value1,-999,NULL,a.num_value1)) AS num_value1,
+      to_number(decode(a.num_value2,-999,NULL,a.num_value2)) AS num_value2,
+      a.date_value1,
+      a.date_value2
+   from system.mview$_adv_filter a, system.mview$_adv_log b, ALL_USERS u
+   WHERE a.filterid# = b.runid#
+   AND b.uname = u.username
+   AND u.user_id = userenv('SCHEMAID');
+
+   COMMENT ON TABLE "MVIEW_FILTER"  IS 'Workload filter records'
+;
+--------------------------------------------------------
+--  DDL for View MVIEW_FILTERINSTANCE
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE NONEDITIONABLE VIEW "MVIEW_FILTERINSTANCE" ("RUNID", "FILTERID", "SUBFILTERNUM", "SUBFILTERTYPE", "STR_VALUE", "NUM_VALUE1", "NUM_VALUE2", "DATE_VALUE1", "DATE_VALUE2") AS 
+  select
+      a.runid# as runid,
+      a.filterid# as filterid,
+      a.subfilternum# as subfilternum,
+      decode(a.subfiltertype,1,'APPLICATION',2,'CARDINALITY',3,'LASTUSE',
+                             4,'FREQUENCY',5,'USER',6,'PRIORITY',7,'BASETABLE',
+                             8,'RESPONSETIME',9,'COLLECTIONID',10,'TRACENAME',
+                             11,'SCHEMA','UNKNOWN') AS subfiltertype,
+      a.str_value,
+      to_number(decode(a.num_value1,-999,NULL,a.num_value1)) AS num_value1,
+      to_number(decode(a.num_value2,-999,NULL,a.num_value2)) AS num_value2,
+      a.date_value1,
+      a.date_value2
+   from system.mview$_adv_filterinstance a;
+
+   COMMENT ON TABLE "MVIEW_FILTERINSTANCE"  IS 'Workload filter instance records'
+;
+--------------------------------------------------------
+--  DDL for View MVIEW_LOG
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE NONEDITIONABLE VIEW "MVIEW_LOG" ("ID", "FILTERID", "RUN_BEGIN", "RUN_END", "TYPE", "STATUS", "MESSAGE", "COMPLETED", "TOTAL", "ERROR_CODE") AS 
+  select
+      m.runid# as id,
+      m.filterid# as filterid,
+      m.run_begin,
+      m.run_end,
+      decode(m.run_type,1,'EVALUATE',2,'EVALUATE_W',3,'RECOMMEND',
+                      4,'RECOMMEND_W',5,'VALIDATE',6,'WORKLOAD',
+                      7,'FILTER','UNKNOWN') AS type,
+      decode(m.status,0,'UNUSED',1,'CANCELLED',2,'IN_PROGRESS',3,'COMPLETED',
+                    4,'ERROR','UNKNOWN') AS status,
+      m.message,
+      m.completed,
+      m.total,
+      m.error_code
+   from system.mview$_adv_log m, all_users u
+   where m.uname = u.username
+   and   u.user_id = userenv('SCHEMAID');
+
+   COMMENT ON TABLE "MVIEW_LOG"  IS 'Advisor session log'
+;
+--------------------------------------------------------
+--  DDL for View MVIEW_RECOMMENDATIONS
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE NONEDITIONABLE VIEW "MVIEW_RECOMMENDATIONS" ("RUNID", "ALL_TABLES", "FACT_TABLES", "GROUPING_LEVELS", "QUERY_TEXT", "RECOMMENDATION_NUMBER", "RECOMMENDED_ACTION", "MVIEW_OWNER", "MVIEW_NAME", "STORAGE_IN_BYTES", "PCT_PERFORMANCE_GAIN", "BENEFIT_TO_COST_RATIO") AS 
+  select
+  t1.runid# as runid,
+  t1.from_clause as all_tables,
+  fact_tables,
+  grouping_levels,
+  query_text,
+  rank# as recommendation_number,
+  action_type as recommended_action,
+  summary_owner as mview_owner,
+  summary_name as mview_name,
+  storage_in_bytes,
+  pct_performance_gain,
+  benefit_to_cost_ratio
+from SYSTEM.MVIEW$_ADV_OUTPUT t1, SYSTEM.MVIEW$_ADV_LOG t2, ALL_USERS u
+where
+  t1.runid# = t2.runid# and
+  u.username = t2.uname and
+  u.user_id = userenv('SCHEMAID') and
+  t1.output_type = 0
+order by t1.rank#;
+
+   COMMENT ON TABLE "MVIEW_RECOMMENDATIONS"  IS 'This view gives DBA access to summary recommendations'
+;
+--------------------------------------------------------
+--  DDL for View MVIEW_WORKLOAD
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE NONEDITIONABLE VIEW "MVIEW_WORKLOAD" ("WORKLOADID", "IMPORT_TIME", "QUERYID", "APPLICATION", "CARDINALITY", "RESULTSIZE", "LASTUSE", "FREQUENCY", "OWNER", "PRIORITY", "QUERY", "RESPONSETIME") AS 
+  select
+  a.collectionid# as workloadid,
+  a.collecttime as import_time,
+  a.queryid# as queryid,
+  a.application,
+  a.cardinality,
+  a.resultsize,
+  a.qdate as lastuse,
+  a.frequency,
+  a.uname as owner,
+  a.priority,
+  a.sql_text as query,
+  a.exec_time as responsetime
+from SYSTEM.MVIEW$_ADV_WORKLOAD A, SYSTEM.MVIEW$_ADV_LOG B, ALL_USERS D
+WHERE a.collectionid# = b.runid#
+AND b.uname = d.username
+AND d.user_id = userenv('SCHEMAID');
+
+   COMMENT ON TABLE "MVIEW_WORKLOAD"  IS 'This view gives DBA access to shared workload'
+;
+--------------------------------------------------------
+--  DDL for View PRODUCT_PRIVS
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE NONEDITIONABLE VIEW "PRODUCT_PRIVS" ("PRODUCT", "USERID", "ATTRIBUTE", "SCOPE", "NUMERIC_VALUE", "CHAR_VALUE", "DATE_VALUE", "LONG_VALUE") AS 
+  SELECT PRODUCT, USERID, ATTRIBUTE, SCOPE,
+         NUMERIC_VALUE, CHAR_VALUE, DATE_VALUE, LONG_VALUE
+  FROM SQLPLUS_PRODUCT_PROFILE
+  WHERE USERID = 'PUBLIC' OR
+        USERID LIKE SYS_CONTEXT('USERENV','CURRENT_USER')
+;
+  GRANT READ ON "PRODUCT_PRIVS" TO PUBLIC;
+--------------------------------------------------------
+--  DDL for View SCHEDULER_JOB_ARGS
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE NONEDITIONABLE VIEW "SCHEDULER_JOB_ARGS" ("OWNER", "JOB_NAME", "ARGUMENT_NAME", "ARGUMENT_POSITION", "ARGUMENT_TYPE", "VALUE", "ANYDATA_VALUE", "OUT_ARGUMENT") AS 
+  SELECT "OWNER","JOB_NAME","ARGUMENT_NAME","ARGUMENT_POSITION","ARGUMENT_TYPE","VALUE","ANYDATA_VALUE","OUT_ARGUMENT" FROM sys.all_scheduler_job_args
+;
+  GRANT SELECT ON "SCHEDULER_JOB_ARGS" TO "SELECT_CATALOG_ROLE";
+--------------------------------------------------------
+--  DDL for View SCHEDULER_PROGRAM_ARGS
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE NONEDITIONABLE VIEW "SCHEDULER_PROGRAM_ARGS" ("OWNER", "PROGRAM_NAME", "ARGUMENT_NAME", "ARGUMENT_POSITION", "ARGUMENT_TYPE", "METADATA_ATTRIBUTE", "DEFAULT_VALUE", "DEFAULT_ANYDATA_VALUE", "OUT_ARGUMENT") AS 
+  SELECT "OWNER","PROGRAM_NAME","ARGUMENT_NAME","ARGUMENT_POSITION","ARGUMENT_TYPE","METADATA_ATTRIBUTE","DEFAULT_VALUE","DEFAULT_ANYDATA_VALUE","OUT_ARGUMENT" FROM sys.all_scheduler_program_args
+;
+  GRANT SELECT ON "SCHEDULER_PROGRAM_ARGS" TO "SELECT_CATALOG_ROLE";
+REM INSERTING into TBL_VOTACIONES
+SET DEFINE OFF;
+REM INSERTING into TBL_VOTACION
+SET DEFINE OFF;
+REM INSERTING into TBL_PARTIDO
+SET DEFINE OFF;
+REM INSERTING into USUARIOS
+SET DEFINE OFF;
+REM INSERTING into TBL_GRAFICA
+SET DEFINE OFF;
+REM INSERTING into TBL_DVOTACIONES
+SET DEFINE OFF;
+REM INSERTING into TBL_CANDIDATOS
+SET DEFINE OFF;
